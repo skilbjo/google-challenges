a6151452a98db36d2217e4b52db7dbfc00e868ca
John Skilbeck <john.skilbeck@gmail.com>
2016-06-13 13:35:48 -0700

    guards

--- guard_game/solution.py
+++ guard_game/solution.py
@@ -1,6 +1,18 @@
+def add(x,y):
+	return x + y
+
+def solve(sequence):
+	sequence = list(str(sequence))
+
+	if len(sequence) == 1:
+		return int(sequence[0])
+	else: 
+		sequence = map(int,sequence)
+		sum = reduce(add, sequence)
+		return solve(sum)
 
 def answer(x):
-	return x
+	return solve(x)
 
 def test(fn,input,output):
 	if (fn(input) == output):
@@ -9,7 +21,8 @@ def test(fn,input,output):
 		print 'Keep trying'
 
 def main():
-	test(answer,7,4)
+	test(answer,1235,2)
+	test(answer,13,4)
 	# answer(7)
 
 if __name__ == '__main__':
